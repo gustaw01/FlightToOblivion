af62c28bc5aa206e8b10e86282eac51411c311a6
Konrad <goostaw.projekt@gmail.com>
2022-08-23 16:15:16 +0200

    scoring system updated

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,23 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int pointsToScore = 50;
     [SerializeField] int hitPoints = 3;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
-    void Start() {
-       scoreBoard = FindObjectOfType<ScoreBoard>();
-       
+    void Start()
+    {
+        AddRigigdbody();
+        parentGameObject = GameObject.FindWithTag("SpawnOnRunetime");
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddRigigdbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other)
@@ -25,17 +33,17 @@ public class Enemy : MonoBehaviour
 
     private void KillEnemy()
     {
+        scoreBoard.IncreaseScore(pointsToScore);
+        scoreBoard.ShowScore();
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
     private void ProcessHit()
     {
-        scoreBoard.IncreaseScore(pointsToScore);
-        scoreBoard.ShowScore();
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         if(hitPoints < 1) { KillEnemy(); }
     }
